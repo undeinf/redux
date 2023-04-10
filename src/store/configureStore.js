1ea818b1a73b0ba4119c88c50270544378b7b2a4
Undeinf <www.undefined@gmail.com>
2023-04-10 22:55:13 +0530

    Added middleware custom and defauls

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,15 +1,28 @@
-import { legacy_createStore as createStore, applyMiddleware } from 'redux'
+import {
+    legacy_createStore as createStore,
+    applyMiddleware
+} from 'redux'
 import thunk from 'redux-thunk'
-import {devToolsEnhancer} from 'redux-devtools-extension'
-import {configureStore} from '@reduxjs/toolkit'
+import {
+    devToolsEnhancer
+} from 'redux-devtools-extension'
+import {
+    configureStore
+} from '@reduxjs/toolkit'
 import taskReducer from './tasks'
 import employeesReducer from './employees'
+import log from '../middleware/log'
+// logger library provided by redux
+import logger from 'redux-logger'
 
-
-const store = configureStore({reducer: {
-    tasks: taskReducer,
-    employees: employeesReducer
-}})
+const store = configureStore({
+    reducer: {
+        tasks: taskReducer,
+        employees: employeesReducer
+    },
+    // added default middleware too to middleware
+    middleware: (getDefaultMiddleware) => [...getDefaultMiddleware(), logger]
+})
 
 // old approach
 // const store = createStore(reducer, applyMiddleware(thunk));
